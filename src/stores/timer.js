7ed3c9476d11dbaf0322a58b0e959b5003d852ca
Alejandro Chavazza <alejandrochavazza@gmail.com>
2024-07-25 12:49:16 +0200

    fixes and sounds

--- src/stores/timer.js
+++ src/stores/timer.js
@@ -1,34 +1,74 @@
 import { defineStore } from 'pinia';
-import { reactive, toRefs, computed } from 'vue';
+import { ref, reactive, toRefs } from 'vue';
+import { v4 as uuidv4 } from 'uuid';
+import { useSound } from '@vueuse/sound';
+import pop1 from '@/assets/sounds/multi-pop-1-188165.mp3';
+import pop2 from '@/assets/sounds/infographic-pop-3-197869.mp3';
+import pop3 from '@/assets/sounds/pop-sound-effect-197846.mp3';
+import pop4 from '@/assets/sounds/bloop-1-184019.mp3';
+
 
 export const useTimerStore = defineStore('timer', () => {
     const state = reactive({
-        timers: [1800, 1200, 900, 5],
+        timers: [
+            { id: uuidv4(), duration: 1800 },
+            { id: uuidv4(), duration: 1200 },
+            { id: uuidv4(), duration: 900 },
+            { id: uuidv4(), duration: 5 },
+        ],
         colors: ["#c2e9f8", "#cac3f7", "#fdf6c2", "#f8caee", "#c6e3a3"],
         thisTime: null,
         newTimer: 60,
         thisColor: null,
     });
+    const volume = ref(0.75);
+    const { play: popRemove } = useSound(pop1, { interrupt: true });
+    const { play: popAdd    } = useSound(pop2, { interrupt: true });
+    const { play: popClick  } = useSound(pop3, {volume, interrupt: true});
+    const { play: popTimer  } = useSound(pop4, { interrupt: true });
 
-    const addTimer = (value) => {
-        const i = state.colors.length;
-        state.timers.push(value);
-        //state.colors.push(state.colors[i % 5]);
+    const setTimer = (id) => {
+        const timer = state.timers.find(timer => timer.id === id);
+        if (timer) {
+            state.thisTime = timer.duration;
+            state.thisColor = state.colors[state.timers.indexOf(timer) % state.colors.length];
+            popTimer();
+        }
     };
 
-    const remove = (index) => {
-        state.timers.splice(index, 1);
+    const addTimer = (value) => {
+        state.timers.push({ id: uuidv4(), duration: value });
+        popAdd();
+    };
+    
+    const removeTimer = (id) => {
+        state.timers = state.timers.filter(timer => timer.id !== id);
+        popRemove();
     };
 
     const setNewTimer = (value) => {
-        console.log(value);
-        state.newTimer = value;
+        state.newTimer = value
+    };
+
+    const incrementTimer = () => {
+        state.newTimer += 10;
+        popClick();
+    };
+    
+    const decrementTimer = () => {
+        if (state.newTimer > 0) {
+            state.newTimer -= 10;
+            popClick();
+        }
     };
 
     return {
         ...toRefs(state),
         addTimer,
-        remove,
+        setTimer,
+        removeTimer,
         setNewTimer,
+        incrementTimer,
+        decrementTimer,
     };
 });
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,144 +1,136 @@
 <script setup>
-import { ref, reactive, onBeforeMount, onMounted, computed, watch } from 'vue'
-import { useDateFormat, usePermission } from '@vueuse/core'
-import { useTimerStore } from "../stores/timer";
+import { ref, computed } from 'vue';
+import { useDateFormat } from '@vueuse/core';
 
 import TimerComponent from "../components/Timer.vue";
-//import timeInput from "../components/timeInput.vue";
-
-import { getNotification } from '@/composables/getNotifications'
-const {notifyMe, permission} = getNotification()
-
-
+import { useTimerStore } from "../stores/timer";
 
 const timerStore = useTimerStore();
+const timers    = computed(() => timerStore.timers);
+const colors    = computed(() => timerStore.colors);
+const thisTime  = computed(() => timerStore.thisTime);
+const thisColor = computed(() => timerStore.thisColor);
+const newTimer  = computed(() => timerStore.newTimer);
 
-//console.log(timerStore.timers)
-//const timer = ref([30,20,15,10])
-const timers    = timerStore.timers
-const colors    = timerStore.colors
-let store       = computed(() => timerStore)
-let thisTime    = computed(() => timerStore.thisTime)
-let thisColor   = computed(() => timerStore.thisColor)
-let newTimer    = computed(() => timerStore.newTimer)
-//let newTimer    = computed(() => timerStore.newTimer)
+const formatter = ref('m:ss');
+const formatted = (i) => useDateFormat(i * 1000, formatter);
+const newFormat = computed(() => useDateFormat(timerStore.newTimer * 1000, formatter).value);
 
-const formatter = ref('m:ss')
-const formatted = (i) => useDateFormat(i*1000, formatter)
-const newFormat = computed(() => useDateFormat(timerStore.newTimer*1000, formatter).value)
-
-
-const setTimer = (i) => {
-    //console.log(i)
-    timerStore.thisTime  = timers[i] ? timers[i] : 10
-    timerStore.thisColor = colors[i] ? colors[i] : colors[0]
-}
-
-const addTimer = (value) => {
-    timerStore.addTimer(value)
-}
-
-const remove = (value) => {
-    //thisTime = null
-    timerStore.remove(value)
-}
-
-const incrementTimer = (value) => {
-    timerStore.newTimer += 10
-}
-
-const decrementTimer = (value) => {
-    if(timerStore.newTimer > 0){
-        timerStore.newTimer -= 10
+/*
+const setTimer = (id) => {
+    const timer = timerStore.timers.find(timer => timer.id === id);
+    if (timer) {
+        timerStore.thisTime = timer.duration;
+        timerStore.thisColor = colors[timerStore.timers.indexOf(timer) % colors.length];
     }
-}
+};
+*/
 
-let val = newFormat
+const setTimer       = (id) => timerStore.setTimer(id);
+const incrementTimer = () => timerStore.incrementTimer();
+const decrementTimer = () => timerStore.decrementTimer();
+
+const removeTimer = (id) => timerStore.removeTimer(id);
+const addTimer = (value) => timerStore.addTimer(value);
+
+
+// para el input
+let val = newFormat;
 const focusIn = (v) => {
-    val = timerStore.newTimer
-    v.target.value = val
-}
+    val = timerStore.newTimer;
+    v.target.value = val;
+};
 
 const focusOut = (v) => {
-    timerStore.setNewTimer(parseInt(v.target.value))
-    val = formatted(timerStore.newTimer).value
-    v.target.value = val
-}
+    timerStore.setNewTimer(parseInt(v.target.value));
+    val = formatted(timerStore.newTimer).value;
+    v.target.value = val;
+};
 
 const isNumber = (evt) => {
-    evt = (evt) ? evt : window.event;
-    var charCode = (evt.which) ? evt.which : evt.keyCode;
+    evt = evt ? evt : window.event;
+    const charCode = evt.which ? evt.which : evt.keyCode;
     if ((charCode > 31 && (charCode < 48 || charCode > 57)) && charCode !== 46) {
         evt.preventDefault();
     } else {
         return true;
     }
+};
+
+</script>
+
+<template>
+    <div class="wrapper">
+        <ul class="grid-wrapper">
+            <TransitionGroup name="list">
+                <li v-for="(timer, i) in timers" :key="timer.id" :class="`grid grid-${i}`" :data-id="timer.id">
+                    <div class="tile" @click.stop="setTimer(timer.id)" :style="`background-color:${colors[timers.indexOf(timer) % colors.length]}`">
+                        <div class="tile-contents">
+                            <span class="number">{{ formatted(timer.duration).value }}</span>
+                        </div>
+                        <div class="tile-contents controls">
+                            <button class="button button-alt" @click.stop="removeTimer(timer.id)" :style="`color:${colors[timers.indexOf(timer) % colors.length]}`">
+                                <span class="material-symbols-outlined"> close </span>
+                            </button>
+                        </div>
+                    </div>
+                </li>
+            </TransitionGroup>
+            <li :class="`grid grid-${timers.length+1} tile`" :style="`background:${colors[bgcolor = timers.length % colors.length]}`" :key="timers.length+1">
+                <div class="tile-contents">
+                    <input class="input" type="text" :value="val" @blur="focusOut($event)" @focus="focusIn($event)" />
+                </div>
+                <div class="tile-contents controls">
+					<button class="button" :disabled="(val == '0:00' ? true : null)" @click="addTimer(newTimer)">
+                        <span class="material-symbols-outlined"> add </span>
+                    </button>
+                    <button class="button button-alt" @click="decrementTimer()" :style="`color:${colors[bgcolor]}`">
+                        <span class="material-symbols-outlined"> arrow_back </span>
+                    </button>
+                    <button class="button button-alt" @click="incrementTimer()" :style="`color:${colors[bgcolor]}`">
+                        <span class="material-symbols-outlined"> arrow_forward </span>
+                    </button>
+                </div>
+            </li>
+        </ul>
+    </div>
+    <Transition name="pop">
+        <TimerComponent v-if="thisTime" :timer="thisTime" :color="thisColor"/>
+    </Transition>
+</template>
+
+<style>
+.list-enter-active,
+.list-leave-active {
+    transition: all 200ms ease;
+}
+/*
+.list-leave-active .tile{
+    transition: all 200ms ease;
+	height:0%;
+	width:0%;
+}*/
+.list-enter,
+.list-enter-to,
+.list-leave-from {
+	opacity: 1;
+    transform: scale(0.95);
+}
+.list-enter-from,
+.list-leave-to {
+    opacity: 0;
+    transform: scale(1.05);
 }
 
-onMounted(() => {
-    //usePermission('push')
-	if (permission) {
-	notifyMe();
-	}
-})
-</script>
-<template>
-	<div class="wrapper">
-		<!-- <div :class="`grid grid-${i}`" v-for="(i, index) in 7" :key="index" :data-id="i"> -->
-		<div :class="`grid grid-${i}`" v-for="(i, index) in timers.length" :key="index" :data-id="i">
-			<div class="tile"  @click.stop="setTimer(index)" :style="`background-color:${colors[index % colors.length]}`">
-			<!-- <a href="#" class="tile" @click="setTimer(index)" :style="`background-color:${store.colors[index]}`"> -->
-				<div class="tile-contents">
-					<!-- {{store.timers[index]}} -->
-					<span class="number">{{formatted(timers[index]).value}}</span>
-					<!-- {{formatted(store.timers[index]).value}} -->
-					<!-- {{store.colors[index]}} -->
-					<!-- {{i}} -->
-				</div>
-				<div class="tile-contents controls">
-					<!-- <button class="button" @click="remove(index)" :style="`color:${store.colors[index % store.colors.length]}`"> -->
-					<button class="button button-alt" @click.stop="remove(index)" :style="`color:${store.colors[index % store.colors.length]}`">
-						<span class="material-symbols-outlined">
-							close
-						</span>
-					</button>
-				</div>
-			</div>
-		</div>
-		<div class="grid">
-			<div class="tile" :style="`background:${colors[bgcolor = timers.length % (colors.length)]}`">
-				<div class="tile-contents">
-					<!-- <timeInput :timer="newTimer"/>  -->
-					<!-- <input class="input" type="text" v-model="formatter" @blur="focusOut" @focus="focusIn" /> -->
-					<input class="input" type="text" :value="val" @blur="focusOut($event)" @focus="focusIn($event)" />
-					<!-- <input class="input" type="number" @keypress="isNumber($event)" v-model="newTimer" @blur="focusOut($event)" @focus="focusIn($event)" /> -->
-					<!-- {{thisTime}} {{thisColor}} -->
-				</div>
-				<div class="tile-contents controls">
-					<div>
-						<button class="button" @click="addTimer(newTimer)">
-							<span class="material-symbols-outlined">
-								add
-							</span>
-						</button>
-					</div>
-					<div>
-						<button class="button button-alt" @click="decrementTimer()"  :style="`color:${colors[bgcolor]}`">
-							<span class="material-symbols-outlined">
-								arrow_back
-							</span>
-						</button>
-						<button class="button button-alt" @click="incrementTimer()"  :style="`color:${colors[bgcolor]}`">
-							<span class="material-symbols-outlined">
-								arrow_forward
-							</span>
-						</button>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-	<Transition>
-		<TimerComponent v-if="thisTime" :timer="thisTime" :color="thisColor"/>
-	</Transition>
-</template>
+.pop-enter-active,
+.pop-leave-active {
+    transition: all 200ms ease;
+    transform: scale(1);
+}
+
+.pop-enter-from,
+.pop-leave-to {
+    opacity: 0;
+    transform: scale(1.05);
+}
+</style>
